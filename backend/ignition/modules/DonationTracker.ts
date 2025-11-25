fef2878d1d4fa6bb8c63cfa6102b54c3f2d12106
Klem <klem86@protonmail.com>
2025-11-25 19:37:24 +0100

    backend::Removed file

--- backend/ignition/modules/DonationTracker.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("DonationTrackerModule", (m) => {
-    const Tracker = m.contract("DonationTracker");
-
-    return {Tracker };
-});
